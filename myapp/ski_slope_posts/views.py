5269b8c7634004a347095b4f65ad549a43446c42
djrich87 <djrich87@gmail.com>
2022-04-11 17:08:56 -0600

    add delete function

--- myapp/ski_slope_posts/views.py
+++ myapp/ski_slope_posts/views.py
@@ -23,3 +23,41 @@ def create_post():
 def ski_slope_post(ski_slope_post_id):
   ski_slope_post = SkiSlopePost.query.get_or_404(ski_slope_post_id)
   return render_template('ski_slope_post.html', title=ski_slope_post.title, date=ski_slope_post.date, post=ski_slope_post)
+
+
+@ski_slope_posts.route('/<int:ski_slope_post_id>/update',methods=['GET','POST'])
+@login_required
+def update(ski_slope_post_id):
+    ski_slope_post = SkiSlopePost.query.get_or_404(ski_slope_post_id)
+
+    if ski_slope_post.author != current_user:
+        abort(403)
+
+    form = SkiSlopePostForm()
+
+    if form.validate_on_submit():
+        ski_slope_post.title = form.title.data
+        ski_slope_post.text = form.text.data
+        db.session.commit()
+        flash('Ski Slope Post Updated')
+        return redirect(url_for('ski_slope_posts.ski_slope_post',ski_slope_post_id=ski_slope_post.id))
+
+    elif request.method == 'GET':
+        form.title.data = ski_slope_post.title
+        form.text.data = ski_slope_post.text
+
+    return render_template('create_post.html',title='Updating',form=form)
+
+
+@ski_slope_posts.route('/<int:ski_slope_post_id>/delete',methods=['GET', 'POST'])
+@login_required
+def delete_post(ski_slope_post_id):
+
+    ski_slope_post = SkiSlopePost.query.get_or_404(ski_slope_post_id)
+    if ski_slope_post.author != current_user:
+      abort(403)
+
+    db.session.delete(ski_slope_post)
+    db.session.commit()
+    flash('Ski Slope Post Deleted')
+    return redirect(url_for('core.index'))
